0c2225d720b8e0fbc9048056a0197c374cbcb66f
spoon lava <lavaspoon@naver.com>
2022-01-09 22:50:19 +0900

    터미널 git commit test

---
+++ src/com/company/algolithm/ch02.java
@@ -0,0 +1,11 @@
+package com.company.algolithm;
+
+public class ch02 {
+    public int solution(){
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println("ch02.main");
+    }
+}
