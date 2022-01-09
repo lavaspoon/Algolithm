6cd2cc9a7b7110bbc0949e395c5bf945c2063e10
spoon lava <lavaspoon@naver.com>
2022-01-09 22:32:09 +0900

    1. 문자찾기 solved

--- src/com/company/algolithm/ch01.java
+++ src/com/company/algolithm/ch01.java
@@ -1,7 +1,34 @@
 package com.company.algolithm;
 
+import java.util.*;
+
+//문자찾기
 public class ch01 {
+    public int solution(String str, char t){
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+        /*
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == t){
+                answer++;
+            }
+        }
+        */
+        for(char x : str.toCharArray()){
+            if(x == t){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ch01 T = new ch01();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        System.out.print(T.solution(str, c));
     }
 }
