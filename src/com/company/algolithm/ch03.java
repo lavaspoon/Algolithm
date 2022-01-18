85f6972ad6cae6a178567f9cab543c3e35dbab14
spoon lava <lavaspoon@naver.com>
2022-01-19 00:08:29 +0900

    3. 문장 속 가장 긴 단어 출력 solved

--- src/com/company/algolithm/ch03.java
+++ src/com/company/algolithm/ch03.java
@@ -1,12 +1,27 @@
 package com.company.algolithm;
 
+import java.util.Scanner;
+
 public class ch03 {
-    public int solution(){
-        int answer = 0;
+    public String solution(String str){
+        String answer = "";
+        int max = 0;
+
+        String[] result = str.split(" ");
+        System.out.println("result = " + result);
+        for(String x : result){
+            if(x.length()>max) {
+                max = x.length();
+                answer = x;
+            }
+        }
         return answer;
     }
     public static void main(String[] args) {
-        System.out.println("Main.main");
+        ch03 T = new ch03();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(T.solution(str));
     }
 }
 
