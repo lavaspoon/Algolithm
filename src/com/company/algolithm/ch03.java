d0d0779b1c5d07e0ed7673208822c8fb28732061
spoon lava <lavaspoon@naver.com>
2022-01-18 23:54:02 +0900

    3. ch03 생성

---
+++ src/com/company/algolithm/ch03.java
@@ -0,0 +1,12 @@
+package com.company.algolithm;
+
+public class ch03 {
+    public int solution(){
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+}
+
