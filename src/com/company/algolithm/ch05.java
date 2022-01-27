22da05bef58e0204e18cf4d74dfc8b63e8b19001
lavaspoon <lavaspoon@naver.com>
2022-01-28 08:42:04 +0900

    ch05 생성

---
+++ src/com/company/algolithm/ch05.java
@@ -0,0 +1,11 @@
+package com.company.algolithm;
+
+public class ch05 {
+    public int solution(){
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+}
