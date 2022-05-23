fcd214381f00303e979c1f1fed3983716eee8c34
spoon lava <lavaspoon@naver.com>
2022-05-23 23:42:24 +0900

    6. 중복문자제거 문제 생성

---
+++ src/com/company/algolithm/ch06.java
@@ -0,0 +1,17 @@
+package com.company.algolithm;
+
+/**
+ * 중복문자제거
+ * > 소문자로 된 한개의 문자열이 입력되며 중복된 문자를 제거하고 출력,
+ * input: ksekkset
+ * output: ksek
+ */
+public class ch06 {
+    public int solution(){
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+}
