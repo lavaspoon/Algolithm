d569613833af27f22ad2d10a3d79f1a24f44cc59
spoon lava <lavaspoon@naver.com>
2022-05-25 22:06:11 +0900

    6. 중복문자제거 문제 해결

--- src/com/company/algolithm/ch06.java
+++ src/com/company/algolithm/ch06.java
@@ -1,5 +1,7 @@
 package com.company.algolithm;
 
+import java.util.Scanner;
+
 /**
  * 중복문자제거
  * > 소문자로 된 한개의 문자열이 입력되며 중복된 문자를 제거하고 출력,
@@ -7,11 +9,20 @@ package com.company.algolithm;
  * output: ksek
  */
 public class ch06 {
-    public int solution(){
-        int answer = 0;
-        return answer;
+    public String solution(String str){
+        String answer = "";
+        String result = "";
+        for(int i=0; i<str.length(); i++){
+            if(!result.contains(String.valueOf(str.charAt(i)))){
+                result += String.valueOf(str.charAt(i));
+            }
+        }
+        return result;
     }
     public static void main(String[] args) {
-        System.out.println("Main.main");
+        ch06 T = new ch06();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(T.solution(str));
     }
 }
