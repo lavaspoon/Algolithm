1491a7c11158c1f5b49f500d04ba9f13c1ba8876
spoon lava <lavaspoon@naver.com>
2022-05-31 23:51:37 +0900

    7. 회문문자열 문제 생성

---
+++ src/com/company/algolithm/ch07.java
@@ -0,0 +1,18 @@
+package com.company.algolithm;
+
+/**
+ * 회문문자열
+ * > 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 한다.
+ * 문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력
+ * input: gooG
+ * output: YES
+ */
+public class ch07 {
+    public int solution(){
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+}
