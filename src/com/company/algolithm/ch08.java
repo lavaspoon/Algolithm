a58db746544494b1f70192d25cb13a2aaef01de1
spoon lava <lavaspoon@naver.com>
2022-06-01 22:39:31 +0900

    8. 팰린드롬 구분 문제 생성

---
+++ src/com/company/algolithm/ch08.java
@@ -0,0 +1,23 @@
+package com.company.algolithm;
+
+import java.util.Scanner;
+
+/**
+ * 팰린드롬 구분하기
+ * > 문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력
+ * (단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.)
+ * input : found7, time: study;  Yduts; emit, 7Dnuof
+ * output: YES
+ */
+public class ch08 {
+    public String solution(String str){
+        String answer = "";
+        return answer;
+    }
+    public static void main(String[] args) {
+        ch08 T = new ch08();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
