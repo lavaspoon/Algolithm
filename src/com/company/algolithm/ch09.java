2f85e5790f1f5ae8d1d6776b0d3be3009edf9b3a
spoon lava <lavaspoon@naver.com>
2022-06-02 22:19:02 +0900

    9. 숫자 추출 문제 생성 및 solved

---
+++ src/com/company/algolithm/ch09.java
@@ -0,0 +1,29 @@
+package com.company.algolithm;
+
+import java.util.Scanner;
+
+/**
+ * 숫자추출
+ * > 문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다.
+ * input: g0en2T0s8eSoft
+ * output: 208
+ */
+public class ch09 {
+    public int solution(String str){
+        String answer = "";
+        char[] s = str.toCharArray();
+        for(int i=0; i<s.length; i++){
+            if(Character.isDigit(s[i])){
+                answer += s[i];
+            }
+        }
+
+        return Integer.parseInt(answer);
+    }
+    public static void main(String[] args) {
+        ch09 T = new ch09();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution(str));
+    }
+}
