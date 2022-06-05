5ebc9b619db849d2a4cd924f8accfb92ad910baa
spoon lava <lavaspoon@naver.com>
2022-06-05 23:55:52 +0900

    10. 가장 짧은 문자거리 문제 생성

---
+++ src/com/company/algolithm/ch10.java
@@ -0,0 +1,18 @@
+package com.company.algolithm;
+
+/**
+ * 가장 짧은 문자거리
+ * >  한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력
+ *    문자열 s와 문자 t가 주어진다. 문자열과 문자는 소문자로만 주어짐
+ * input: teachermode e
+ * output: 1 0 1 2 1 0 1 2 2 1 0
+ */
+public class ch10 {
+    public int solution(){
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+}
