584c35954639f994597092d0c7c8060dfc242d63
spoon lava <lavaspoon@naver.com>
2022-06-06 23:27:33 +0900

    10. 가장 짧은 문자거리 문제 생성 solved

--- src/com/company/algolithm/ch10.java
+++ src/com/company/algolithm/ch10.java
@@ -1,5 +1,7 @@
 package com.company.algolithm;
 
+import java.util.Scanner;
+
 /**
  * 가장 짧은 문자거리
  * >  한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력
@@ -8,11 +10,38 @@ package com.company.algolithm;
  * output: 1 0 1 2 1 0 1 2 2 1 0
  */
 public class ch10 {
-    public int solution(){
-        int answer = 0;
+    public int[] solution(String s, char t){
+        int[] answer = new int[s.length()];
+        int p = 1000;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)==t) {
+                p = 0;
+                answer[i]=p;
+            }else {
+                p++;
+                answer[i]=p;
+            }
+        }
+        p=1000;
+        for(int i=s.length()-1;i>=0;i--) {
+            if(s.charAt(i)==t) {
+                p = 0;
+            }else {
+                p++;
+                answer[i]=Math.min(answer[i], p);
+            }
+        }
+
         return answer;
     }
     public static void main(String[] args) {
-        System.out.println("Main.main");
+        ch10 T = new ch10();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+
+        for(int x: T.solution(str, c)){
+            System.out.print(x+ " ");
+        }
     }
 }
