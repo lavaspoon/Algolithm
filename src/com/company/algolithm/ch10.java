0c2fbad913bf506af4d1ffc1d2f98731a54a6bf2
spoon lava <lavaspoon@naver.com>
2022-06-11 00:19:56 +0900

    10. 가장 짧은 문자거리 - resolved

--- src/com/company/algolithm/ch10.java
+++ src/com/company/algolithm/ch10.java
@@ -13,14 +13,27 @@ public class ch10 {
     public int[] solution(String s, char t){
         int[] answer = new int[s.length()];
         int p = 1000;
-
-        int count = 0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == t){
-                count++;
+        //s 뒤의 문자가 s와 떨어진 거리를 구하기위해 왼쪽에서 탐색 1001 1002 1003 1004 0 1 2 3 4
+        for(int i=0;i<s.length();i++) {
+            //배열의 index가 입력된 문자인지 체크
+            if (s.charAt(i) == t) { //한번에 발견된 경우 0 저장
+                p = 0;
+            } else { //입력된 문자가 아닌 경우 p 증가
+                p++;
+                answer[i] = p;
             }
         }
-        System.out.println("count = " + count);
+        p = 1000;
+        //s 의 문자가 s와 떨어진 거리를 구하기위해 오른쪽에서 탐색
+        for(int i=s.length()-1;i>=0;i--) {
+            //배열의 index가 입력된 문자인지 체크
+            if (s.charAt(i) == t) { //한번에 발견된 경우 0 저장
+                p = 0;
+            } else { //입력된 문자가 아닌 경우 p 증가, 단 이미 앞에서 세팅한 값이 있기 때문에 비교해서 작은 값만 넣어준다.
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
 
         return answer;
     }
