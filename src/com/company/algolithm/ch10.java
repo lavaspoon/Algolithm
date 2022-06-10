9013b1370e9ec64f20171ac461fabc156d080c91
spoon lava <lavaspoon@naver.com>
2022-06-10 23:56:09 +0900

    10. 가장 짧은 문자거리 - resolving

--- src/com/company/algolithm/ch10.java
+++ src/com/company/algolithm/ch10.java
@@ -13,24 +13,14 @@ public class ch10 {
     public int[] solution(String s, char t){
         int[] answer = new int[s.length()];
         int p = 1000;
-        for(int i=0;i<s.length();i++) {
-            if(s.charAt(i)==t) {
-                p = 0;
-                answer[i]=p;
-            }else {
-                p++;
-                answer[i]=p;
+
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == t){
+                count++;
             }
         }
-        p=1000;
-        for(int i=s.length()-1;i>=0;i--) {
-            if(s.charAt(i)==t) {
-                p = 0;
-            }else {
-                p++;
-                answer[i]=Math.min(answer[i], p);
-            }
-        }
+        System.out.println("count = " + count);
 
         return answer;
     }
