63060286e5f8da05343132b612886511088ed54c
lavaspoon <lavaspoon@naver.com>
2022-06-24 10:00:55 +0900

    ch12 암호화 문제 생성

---
+++ src/com/company/algolithm/ch12.java
@@ -0,0 +1,28 @@
+package com.company.algolithm;
+
+import java.util.Scanner;
+
+/**
+ * 암호화 알고리즘
+ * 1. “#*****#”를 일곱자리의 이진수로 바꿉니다. #은 이진수의 1로, *이진수의 0으로 변환합니다. 결과는 “1000001”로 변환됩니다.
+ * 2. 바뀐 2진수를 10진수화 합니다. “1000001”을 10진수화 하면 65가 됩니다.
+ * 3. 아스키 번호가 65문자로 변환합니다. 즉 아스크번호 65는 대문자 'A'입니다.
+ * 참고로 대문자들의 아스키 번호는 'A'는 65번, ‘B'는 66번, ’C'는 67번 등 차례대로 1씩 증가하여 ‘Z'는 90번입니다.
+ * 4. 첫 줄에는 보낸 문자의 개수(10을 넘지 안습니다)가 입력된다. 다음 줄에는 문자의 개수의 일곱 배 만큼의 #또는 * 신호가 입력됩니다.
+ * 항상 대문자로 해석할 수 있는 신호를 보낸다고 가정합니다.
+ * input: 4, #****###**#####**#####**##**
+ * output: COOL
+ */
+public class ch12 {
+    public String solution(int n, String str){
+        String answer = "";
+        return answer;
+    }
+    public static void main(String[] args) {
+        ch12 T = new ch12();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        System.out.println(T.solution(n, str));
+    }
+}
