3896ca071f4c536ea69025df33521e903e954676
spoon lava <lavaspoon@naver.com>
2022-06-26 20:25:08 +0900

    ch12 암호화 알고리즘 solved

--- src/com/company/algolithm/ch12.java
+++ src/com/company/algolithm/ch12.java
@@ -16,6 +16,12 @@ import java.util.Scanner;
 public class ch12 {
     public String solution(int n, String str){
         String answer = "";
+        for(int i=0; i<n; i++){
+            String temp = str.substring(0,7).replace('#','1').replace('*','0');
+            int num = Integer.parseInt(temp,2);
+            answer += (char)num;
+            str = str.substring(7);
+        }
         return answer;
     }
     public static void main(String[] args) {
