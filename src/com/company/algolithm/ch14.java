f744bd1a514386713809099d387c3ffd33675bb3
lava <lavaspoon@naver.com>
2023-07-15 23:43:02 +0900

    ch14 add main method

--- src/com/company/algolithm/ch14.java
+++ src/com/company/algolithm/ch14.java
@@ -1,5 +1,12 @@
 package com.company.algolithm;
 
+import java.util.Scanner;
+
 //git commit
 public class ch14 {
+
+    public static void main(String[] args) {
+        ch13 T = new ch13();
+        Scanner sc = new Scanner(System.in);
+    }
 }
